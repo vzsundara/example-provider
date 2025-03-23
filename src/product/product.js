725f5d90798a338d10529639015d3943ba72d82e
sundara <vzsundara@zohomail.com>
2025-03-23 19:07:39 -0400

    Introduced variable dependancy

--- src/product/product.js
+++ src/product/product.js
@@ -4,6 +4,7 @@ class Product {
         this.type = type;
         this.name = name;
         this.version = version;
+        this.dependencies = [];
     }
 }
 
--- src/product/product.repository.js
+++ src/product/product.repository.js
@@ -4,9 +4,10 @@ class ProductRepository {
 
     constructor() {
         this.products = new Map([
-            ["09", new Product("09", "CREDIT_CARD", "Gem Visa", "v1")],
-            ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1")],
-            ["11", new Product("11", "PERSONAL_LOAN", "MyFlexiPay", "v2")],
+            ["09", new Product("09", "CREDIT_CARD", "Gem Visa", "v1", ["100"])],
+            ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1", ["110"])],
+            ["11", new Product("11", "PERSONAL_LOAN", "MyFlexiPay", "v2", [])],
+            ["12", new Product("12", "PERSONAL_LOAN", "DirectPay", "v3", ["100", "110"])],
         ]);
     }
 
